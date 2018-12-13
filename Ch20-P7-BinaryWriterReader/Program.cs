a9eba6eb36d69572a6da5779da4495e1512998ef
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-13 12:56:26 +0500

    Project Created

---
+++ Ch20-P7-BinaryWriterReader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch20_P7_BinaryWriterReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
